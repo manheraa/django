81e762aab9c912bf8318348e516503fdb56f9fc1
sachetan <manuheralagi4@gmail.com>
2023-01-06 00:42:57 +0530

    thsid

--- myapp/settings.py
+++ myapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tql!j=53j-+c0vj$m^%-m4cl#gw73zmujz-hu2bsr*g3ym$3z=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1",'sampledomain.com']
+ALLOWED_HOSTS = ['.vercel.app',".now.sh"]
 
 
 # Application definition
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS=os.path.join(BASE_DIR,"static")
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles_build","static")
